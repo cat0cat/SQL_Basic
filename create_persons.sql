bdf2d79295992b2dedc486aa0189f39592157def
Natalia Zubareva <n.zubareva@inbox.ru>
2022-07-23 13:30:57 +0300

    Initial commit

---
+++ create_persons.sql
@@ -0,0 +1,11 @@
+create schema netology;
+
+create table persons
+(
+    name varchar(255),
+    surname varchar(255),
+    age int check (age > 0),
+    phone_number int,
+    city_of_living varchar(255),
+    primary key (name, surname, age)
+);
